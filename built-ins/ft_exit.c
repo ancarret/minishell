241800e4d07273d5ba62bf72b94fa534f18779b1
ancarret <andrescarjim8@gmail.com>
2025-07-09 15:12:18 +0200

    cambio de cabeceras

--- built-ins/ft_exit.c
+++ built-ins/ft_exit.c
@@ -6,8 +6,24 @@
 /*   By: ancarret <ancarret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 11:07:12 by ancarret          #+#    #+#             */
-/*   Updated: 2025/07/07 10:25:08 by ancarret         ###   ########.fr       */
+/*   Updated: 2025/07/09 13:31:38 by ancarret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+/*
+Casos a tener en cuenta del exit:
+Entrada	        Salida	                            Código salida	    Termina Shell
+exit	        exit	                            Último $?	        Sí
+exit 42	        exit	                            42	                Sí
+exit foo	    exit + error numérico	            2	                Sí
+exit 1 2	    exit + error demasiados args	    1	                No
+exit foo bar	exit + error numérico	            2	                Sí
+exit >max_int	exit + error numérico	            2	                Sí
+*/
+
+int ft_exit(char **args)
+{
+    
+}
